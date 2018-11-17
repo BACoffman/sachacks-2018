c65aa3a04451913a9fb63497ab9df2d1a5071426
kuesa <me@noahgrove.me>
2018-11-17 11:14:46 -0800

    Initial development stage

---
+++ source/Player.hx
@@ -0,0 +1,13 @@
+package;
+
+import flixel.FlxSprite;
+
+class Player extends FlxSprite {
+    override public function create():Void {
+        super.create();
+    }
+
+    override public function update(elapsed:Float):Void {
+		super.update(elapsed);
+	}
+}
