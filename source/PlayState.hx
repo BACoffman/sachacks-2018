f066afd1a7c79667d6f917cade10051cdd83fc6b
kuesa <me@noahgrove.me>
2018-11-17 19:48:36 -0800

    Added rocet and player animations

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,13 +1,72 @@
 package;
 
 import flixel.FlxState;
+import flixel.group.FlxGroup;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxColor;
+import flixel.util.FlxSpriteUtil;
 
 class PlayState extends FlxState {
+
+	public static var rockets:FlxTypedGroup<Rocket>;
+	public var player1:Player;
+	public var player2:Player;
+	public var floor:FlxSprite;
+
 	override public function create():Void {
+		
+		// Remove cursor
+		FlxG.mouse.visible = false;
+
+		// Level
+		floor = new FlxSprite(0, 300);
+		floor.makeGraphic(640, 60, FlxColor.PINK);
+		floor.immovable = true;
+		add(floor);
+		
+
+		// Players
+		player1 = new Player(100, 0, true);
+		player2 = new Player(500, 0, false);
+		add(player1);
+		add(player2);
+
+		rockets = new FlxTypedGroup<Rocket>(16);
+
+		var rocket:Rocket;
+		for (i in 0...16) {
+			rocket = new Rocket(-100, -100);
+			rocket.exists = false;
+
+			rockets.add(rocket);
+		}
+
+		add(rockets);
+
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
+
+		//FlxG.camera.follow(player1);
+
+		// Screen Wrapping
+		FlxSpriteUtil.screenWrap(player1, true, true, false, false);
+		FlxSpriteUtil.screenWrap(player2, true, true, false, false);
+
+		// Collisions
+		FlxG.collide(player1, player2);
+		FlxG.collide(player1, floor);
+		FlxG.collide(player2, floor);
+
+		FlxG.overlap(player1, rockets, endGame);
+		FlxG.overlap(player2, rockets, endGame);
+
+	}
+
+	private function endGame(player:Player, rocket:Rocket){
+
 	}
 }
---
+++ source/Rocket.hx
@@ -0,0 +1,26 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.util.FlxColor;
+import flixel.FlxG;
+
+
+class Rocket extends FlxSprite {  
+    override public function new(X:Int, Y:Int):Void {
+
+        super(X, Y);
+
+        // Graphics
+        makeGraphic(4, 8, FlxColor.LIME);
+        
+    }
+
+    override public function update(elapsed:Float):Void {
+        if(getScreenPosition().x < -16 || getScreenPosition().x > FlxG.width + 16 || getScreenPosition().y > FlxG.height + 16 || touching != 0) {
+            kill();
+        } else {
+            y += 6;
+            super.update(elapsed);
+        }
+	}
+}
